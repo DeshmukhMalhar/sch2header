7655816c58c6af2cabec43e0a18f6b2c52f58d8b
Malhar Deshmukh <malhar_deshmukh@outlook.com>
2019-05-27 10:11:05 +0530

    trying to extract footprint mapping section

--- readfile.py
+++ readfile.py
@@ -25,7 +25,7 @@ for line in lines:
             # print(component_value)
             list_component_value.append(component_value)
 list_components = [list_component_id, list_component_value]
-print(list_components)
+# print(list_components)
 
 if(given_id in list_component_id):
     where_is_IC = list_component_id.index(given_id)
@@ -33,3 +33,20 @@ if(given_id in list_component_id):
 given_value = list_component_value[where_is_IC]
 
 print("FOUND: " + given_value)
+where_footprint = 0
+for line in lines:
+    where_footprint += 1
+    if(re.match(".*"+given_value+".*", line)):
+        print(line)
+        print("Line #: "+str(where_footprint))
+
+fp_lib_sec_regex = "\s*\(libpart \(lib[.]*\)[.]*\(part.*\).*\n\s*\(aliases.*\n [.\n]*\)"
+
+
+'''
+(libpart(lib microcontrollers)(part ATtiny2313V-10SU)
+ (aliases
+    (alias ATtiny2313-20SU)
+    (alias ATtiny2313A-SU)
+  (alias ATtiny4313-SU))
+'''
