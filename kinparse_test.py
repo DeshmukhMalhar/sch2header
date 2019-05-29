ca2dd551f877c8a45e8a084fccb0a8f3dc328c4b
deshmukhmalhar <malhar_deshmukh@outlook.com>
2019-05-29 18:47:22 +0530

    tried kinparse, unable to get complete part

--- kinparse_test.py
+++ kinparse_test.py
@@ -1,16 +1,19 @@
 #documentation https://xesscorp.github.io/kinparse/docs/_build/singlehtml/index.html
 
 from kinparse import parse_netlist
-netlist = parse_netlist('kicad-project/USBtiny_custom/USBtiny_custom.net')
+
+netlist = parse_netlist('kicad-project/test_project_with_uC/test_project_with_uC.net')
+
 print(netlist.version)
 print("netlist date: "+netlist.date)
-# for part in netlist.parts:
-#     print(part)
-# print(netlist.parts)
-for net in netlist.nets:
-    # print(net)
-    print(netlist.pins)
-    # for pin in netlist.pins:
-        # print(pin)
-# print(netlist.nets)
-# print(type(netlist.net))
+
+cpu_refs = ['U1']
+cpu_values = []
+
+for part in netlist.parts:
+    if part.ref in cpu_refs:
+        cpu_values.append(part.value)
+
+print(cpu_values)
+
+print(netlist.nets)
